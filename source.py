8d84180359fdb8688d72611a42d8a6070fda98d1
aaron <aaron.wolf@gmail.com>
2020-06-01 16:24:48 +0300

    begin implementation of PythonTEX for sources

--- source.py
+++ source.py
@@ -3,13 +3,11 @@ import json
 # If you want to change to a different type of verse
 # i.e. without teamim, or without nekudot
 # change the file path below.
-def source(book, chapter, verse = None):
+def source(book, chapter, verse = 0):
     file_path = f"./sources/tanakh/{book}/teamim.json"
     with open(file_path) as json_file:
         data = json.load(json_file)
-        if verse is None:
+        if verse == 0:
             return data["text"][chapter-1]
         else:
             return data["text"][chapter-1][verse-1]
-
-print(source('genesis',1))
