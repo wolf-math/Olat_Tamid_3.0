13d19dd568eee95b7e068b01c6c9f27bd24e8fb2
aaron <aaron.wolf@gmail.com>
2020-06-10 12:14:39 +0300

    cleanup

--- source.py
+++ source.py
@@ -11,3 +11,14 @@ def source(book, chapter, verse = 0):
             return data["text"][chapter-1]
         else:
             return data["text"][chapter-1][verse-1]
+
+
+# latex_jinja_env = jinja2.Environment(
+# 	block_start_string = '\BLOCK{', block_end_string = '}',
+# variable_start_string = '\VAR{', variable_end_string = '}', 
+# comment_start_string = '\#{', comment_end_string = '}', 
+# line_statement_prefix = '%%', 
+# line_comment_prefix = '%#', 
+# trim_blocks = True, 
+# autoescape = False, 
+# loader = jinja2.PackageLoader('app', 'templates'))
