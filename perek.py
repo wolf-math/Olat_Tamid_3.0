dd0e4efde6a71a43fcab51ddf79c2ef9142f7cd7
aaron <aaron.wolf@gmail.com>
2020-10-19 17:03:20 +0300

    added script to print chapter of tanakh

---
+++ perek.py
@@ -0,0 +1,14 @@
+import json
+
+# If you want to change to a different type of verse
+# i.e. without teamim, or without nekudot
+# change the file path below.
+def perek(book, chapter):
+    chapter -= 1
+    file_path = f"./sources/tanakh/{book}/teamim.json"
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+        print(" ".join(data["text"][chapter]))
+
+
+# $ python -c 'import foo; print foo.hello()'
