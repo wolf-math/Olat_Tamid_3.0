743c0aa8495d8775508c878721980c283da7bde7
aaron <aaron.wolf@gmail.com>
2020-10-19 17:31:55 +0300

    added single and multiple pasuk printing

--- pasuk.py
+++ pasuk.py
@@ -2,10 +2,13 @@ import json
 
 # If you want to change to a different type of verse
 # i.e. without teamim, or without nekudot change the file path below.
-def perek(book, chapter):
+# add extra arg to define how many pesukim to print.
+def pasuk(book, chapter, verse, number = 1):
     chapter -= 1
+    verse -= 1
     file_path = f"./sources/tanakh/{book}/teamim.json"
     with open(file_path) as json_file:
         data = json.load(json_file)
-        print(" ".join(data["text"][chapter]))
-
+        print(''.join(data["text"][chapter][verse: verse+number]))
+        
+# $ python -c 'import foo; print foo.hello()'
