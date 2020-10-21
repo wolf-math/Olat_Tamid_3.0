8ce02e62189b515de412ea3bbfd4392f832f0ef2
aaron <aaron.wolf@gmail.com>
2020-10-21 16:16:06 +0300

    tried to add source macro to python script

--- verse.py
+++ verse.py
@@ -3,12 +3,16 @@ import json
 # If you want to change to a different type of verse
 # i.e. without teamim, or without nekudot change the file path below.
 # add number as 3rd param to define how many pesukim to print.
-def pasuk(book, chapter, verse, number = 1):
+def verse(book, chapter, verse, number = 1):
     chapter -= 1
     verse -= 1
     file_path = f"./sources/tanakh/{book}/teamim.json"
     with open(file_path) as json_file:
         data = json.load(json_file)
-        print(''.join(data["text"][chapter][verse: verse+number]))
+        # Double curly bracket escapes from the f-string
+        #source = f"\source{{ {data["heTitle"]}, {chapter+1} }}"
+        heTitle = data["heTitle"]
+        source = "\source{{ {heTitle} }}, {chapter+1}"
+        print(source, ''.join(data["text"][chapter][verse: verse+number]))
         
 # $ python -c 'import foo; print foo.hello()'
